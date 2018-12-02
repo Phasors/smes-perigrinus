5b418ca425b91e803f21f919472f681aa5f6cb33
Sai-PC\Sai <michelleannesayson24@gmail.com>
2018-12-03 03:33:37 +0800

    Remove student-sample

--- student-sample/profile/nav-pills.php
+++
@@ -1,8 +0,0 @@
-   <nav>
-        <div class="nav nav-tabs nav-tabs-cst" id="nav-tab" role="tablist">
-            <a class="nav-item nav-link nav-cst active" id="nav-personal-tab" data-toggle="tab" href="#nav-personal" role="tab" aria-controls="nav-employee" aria-selected="true">PERSONAL DATA</a>
-            <a class="nav-item nav-link nav-cst" id="nav-family-back-tab" data-toggle="tab" href="#nav-family-back" role="tab" aria-controls="nav-family-back" aria-selected="false">FAMILY BACKGROUND</a>
-            <a class="nav-item nav-link nav-cst" id="nav-educ-attn-tab" data-toggle="tab" href="#nav-educ-attn" role="tab" aria-controls="nav-educ-attn" aria-selected="false">EDUCATIONAL ATTAINMENT</a>
-            <a class="nav-item nav-link nav-cst" id="change-pass-tab" data-toggle="tab" href="#change-pass" role="tab" aria-controls="change-pass" aria-selected="false">CHANGE PASSWORD</a>
-        </div>
-    </nav>
